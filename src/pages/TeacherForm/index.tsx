6440af67426d12975dac496a47beab5150fd6f3d
Mayko Douglas <maykodouglasbr@outlook.com>
2020-08-03 18:35:45 -0300

    Estrutura FrontEnd

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+function TeacherForm() {
+  return (
+    <div id="page-teacher-list" className="container">
+      
+    </div>
+  );
+};
+
+export default TeacherForm;
